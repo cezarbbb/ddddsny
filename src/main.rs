e2de0c0706046d602b50e1b3f4fcf2e5f578b4b6
cezarbbb <btq19991016@gmail.com>
2023-12-15 14:47:53 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,28 @@ use std::io::{self, Write};
 use termion::{clear, cursor};
 use std::env;
 
+enum DuffyAndFriends {
+    Olu,
+    LinaBell,
+    CookieAnn,
+}
+
 struct Config {
-    character: String
+    character: DuffyAndFriends
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = parse_config(&args);
     let character = config.character;
-    if character != "olu" {
-        panic!("Sorry, the character you choose is not avaliable now!")
-    }
+    let file_name = match character {
+        DuffyAndFriends::Olu => "examples/Olu.gif",
+        DuffyAndFriends::CookieAnn => "examples/CookieAnn.gif",
+        DuffyAndFriends::LinaBell => "examples/LinaBell.gif",
+        _ => panic!("Sorry, the character you choose is not avaliable now!"),
+    };
 
-    let file = File::open("examples/Olu.gif").unwrap();
+    let file = File::open(file_name).unwrap();
 
     let decoder = GifDecoder::new(file).unwrap();
     let frames = decoder.into_frames();
@@ -27,7 +36,7 @@ fn main() {
 
     for frame in frames {
         let frame = frame.unwrap().into_buffer();
-        let frame = resize_image(&frame, 64, 64);
+        let frame = resize_image(&frame,32, 32);
         let frame_string = convert_frame_to_string(&frame);
         write!(handle, "{}{}", clear::All, cursor::Goto(1, 1)).unwrap();
         handle.flush().unwrap();
@@ -37,14 +46,19 @@ fn main() {
 }
 
 fn parse_config(args: &[String]) -> Config {
-    let default = "olu".to_string();
+    let default = DuffyAndFriends::Olu;
     let mut character = default;
     if args.len() >= 2 {
-        character = args[1].clone();
-        println!("Selecting character: {}!", character);
+        character = match &args[1][..] {
+            "olu" => DuffyAndFriends::Olu,
+            "linabell" => DuffyAndFriends::LinaBell,
+            "cookieann" => DuffyAndFriends::CookieAnn,
+            _ => panic!("Sorry, the character {} you choose is not avaliable now!", &args[1]),
+        };
+        println!("Selecting character: {}!", &args[1]);
     }
     else { 
-        println!("No character selected yet. Selecting default character: {}!", character);
+        println!("No character selected yet. Selecting default character!");
     }
     Config { character }
 }
@@ -55,16 +69,36 @@ fn resize_image(image: &image::RgbaImage, width: u32, height: u32) -> image::Rgb
 }
 
 
+// fn convert_frame_to_colored_string(frame: &image::RgbImage) -> String {
+//     let mut result = String::new();
+//     for y in 0..frame.height() {
+//         for x in 0..frame.width() {
+//             let pixel = frame.get_pixel(x, y);
+//             let (r, g, b) = (pixel[0], pixel[1], pixel[2]);
+//             let color_code = format!("\x1b[48;2;{};{};{}m  ", r, g, b);
+//             result.push_str(&color_code);
+//         }
+//         result.push_str("\x1b[0m\n");
+//     }
+//     result
+// }
+
 fn convert_frame_to_string(frame: &image::RgbImage) -> String {
     let mut result = String::new();
     for y in 0..frame.height() {
         for x in 0..frame.width() {
             let pixel = frame.get_pixel(x, y);
             let (r, g, b) = (pixel[0], pixel[1], pixel[2]);
-            let color_code = format!("\x1b[48;2;{};{};{}m  ", r, g, b);
-            result.push_str(&color_code);
+            let brightness = ((r as u32 + g as u32 + b as u32) / 3) as u8;
+            let ascii_char = match brightness {
+                0..=64 => " ",
+                65..=128 => ".",
+                129..=192 => "*",
+                _ => "#",
+            };
+            result.push_str(ascii_char);
         }
-        result.push_str("\x1b[0m\n");
+        result.push('\n');
     }
     result
 }
